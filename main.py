a533f2f5f87094d520faf52952b0eda663591c4e
Shubham Prasad <shubhamhcs@gmail.com>
2024-09-18 21:54:43 +0530

    Added main funciton to call functions from differnt file

---
+++ main.py
@@ -0,0 +1,11 @@
+from functions.word_count_sorted import check_word_count
+
+def main():
+    sentence = "which is better python 2 or python 3"
+    result = check_word_count(sentence)
+    print(result)
+
+
+
+if __name__ == "__main__":
+    main()
