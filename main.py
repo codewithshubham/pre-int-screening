2bb7ce9de3209535ae6a0f71fc8b59c4aa8e4ffb
Shubham Prasad <shubhamhcs@gmail.com>
2024-09-19 23:43:46 +0530

    Added feature to take inputs from input.txt and return frequency of words in each line

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from functions.word_count_sorted import check_word_count
 
 def main():
+    
     sentence = "which is better python 2 or Python 3"
     result = check_word_count(sentence)
     print(result)
+
+    # Sending inputs from file to run some test scenarios
+    with open("test_inputs/inputs.txt", "r") as file:
+        sentence = file.readlines()
+    
+    for words in sentence:
+        print(f" {words} {check_word_count(words)}")
 
 
 
