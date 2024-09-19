b75848963e69f28be0604f0ec903f9e4f705ca34
Shubham Prasad <shubhamhcs@gmail.com>
2024-09-19 23:32:12 +0530

    Added logic to remove white spaces

--- functions/word_count_sorted.py
+++ functions/word_count_sorted.py
@@ -1,10 +1,11 @@
 def check_word_count(words):
 
-    word_split = words.lower().split(" ")   # Generate individual words and store it in list
-    word_frequency = {}         
+    word_split = words.lower().split(" ")  # Generate individual words and store it in list
+    word_split = [word.strip() for word in word_split if word.strip()]   # Added code to update list of words without whitespcaes
+    word_frequency = {}
 
-    for word in sorted(word_split):
-        if word not in word_frequency:
+    for word in sorted(word_split) :
+        if word not in word_frequency :
             word_frequency[word] = 1 
         else:
             word_frequency[word] = word_frequency[word] + 1 
